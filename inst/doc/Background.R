2b08cf68f4166454dd12d9db1c649d5794719822
Matthew Dick <mgmdick@gmail.com>
2019-04-26 10:23:04 +1000

    Initial commit

---
+++ inst/doc/Background.R
@@ -0,0 +1,6 @@
+## ---- echo=TRUE, results='asis',fig.width=8, fig.height=6,fig.align="center", message=FALSE, warning=FALSE----
+
+library(lastools)
+lastools::las_plot(lastools::example_las_obj)
+
+
